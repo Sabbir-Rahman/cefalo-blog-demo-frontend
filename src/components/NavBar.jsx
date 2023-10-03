e494102a12bb4e2aed201d2ed7a705dacd854834
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2023-10-03 16:56:59 +0600

    navbar work started

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,20 @@
+const NavBar = () => {
+  return (
+    <nav className="bg-white border-2 border-gray-300 dark:bg-gray-900">
+      <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
+        <a href="https://flowbite.com/" className="flex items-center">
+          <img
+            src="https://flowbite.com/docs/images/logo.svg"
+            className="h-8 mr-3"
+            alt="Flowbite Logo"
+          />
+          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
+            Flowbite
+          </span>
+        </a>
+      </div>
+    </nav>
+  );
+};
+
+export default NavBar;
