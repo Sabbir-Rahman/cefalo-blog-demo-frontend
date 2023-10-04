99ae87ec60e30aa09686e0fffaf2edf2846defa4
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2023-10-04 10:53:12 +0600

    primary navbar created

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,81 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faBlog } from '@fortawesome/free-solid-svg-icons';
+
+
 const NavBar = () => {
   return (
-    <nav className="bg-white border-2 border-gray-300 dark:bg-gray-900">
+    <nav className="bg-white border-2 border-gray-300 dark:bg-navy dark:border-none">
       <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
-        <a href="https://flowbite.com/" className="flex items-center">
-          <img
-            src="https://flowbite.com/docs/images/logo.svg"
-            className="h-8 mr-3"
-            alt="Flowbite Logo"
-          />
-          <span className="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">
-            Flowbite
-          </span>
+        <a href="/" className="flex items-center">
+        <FontAwesomeIcon icon={faBlog} style={{color: "#35a29f",}} />
+        <span className="self-center text-2xl font-semibold whitespace-nowrap text-teal dark:text-mint">&nbsp;Bloggie</span>
         </a>
+        
+        <div className="flex md:order-2">
+          <button
+            type="button"
+            className="text-white bg-dark-teal hover:bg-teal focus:ring-4 focus:outline-none focus:ring-mint font-medium rounded-lg text-sm px-4 py-2 text-center mr-3 md:mr-0 dark:text-navy dark:bg-mint dark:hover:bg-dark-mint dark:focus:ring-mint"
+          >
+            Write Blog
+          </button>
+          <button
+            data-collapse-toggle="navbar-cta"
+            type="button"
+            className="inline-flex items-center p-2 w-10 h-10 justify-center text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            aria-controls="navbar-cta"
+            aria-expanded="false"
+          >
+            <span className="sr-only">Open main menu</span>
+            <svg
+              className="w-5 h-5"
+              aria-hidden="true"
+              xmlns="http://www.w3.org/2000/svg"
+              fill="none"
+              viewBox="0 0 17 14"
+            >
+              <path
+                stroke="currentColor"
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M1 1h15M1 7h15M1 13h15"
+              />
+            </svg>
+          </button>
+          <a href="#" className="text-lg mt-1 ml-5  text-dark-teal dark:text-mint hover:underline">Login</a>
+        </div>
+        <div
+          className="items-center justify-between hidden w-full md:flex md:w-auto md:order-1"
+          id="navbar-cta"
+        >
+          <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:flex-row md:space-x-8 md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-white bg-blue-700 rounded md:bg-transparent md:text-dark-teal md:p-0 md:dark:text-mint"
+                aria-current="page"
+              >
+                Dashboard
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal md:p-0 md:dark:hover:text-teal dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                About
+              </a>
+            </li>
+            <li>
+              <a
+                href="#"
+                className="block py-2 pl-3 pr-4 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-teal md:p-0 md:dark:hover:text-teal dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              >
+                Contact
+              </a>
+            </li>
+          </ul>
+        </div>
       </div>
     </nav>
   );
--- tailwind.config.js
+++ tailwind.config.js
@@ -3,7 +3,16 @@ export default {
   content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
   darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        navy: '#071952',
+        'dark-teal': '#0B666A',
+        teal: '#35A29F',
+        mint: '#97FEED',
+        'dark-mint': '#8ad6cc',
+      },
+    },
   },
+
   plugins: [],
 };
