c66375e75ac5637d1643185596a8918aa05d9b90
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2023-10-05 09:25:21 +0600

    input validation added to login modal

--- src/components/LoginModal.jsx
+++ src/components/LoginModal.jsx
@@ -1,8 +1,24 @@
 /* eslint-disable react/prop-types */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faXmark } from '@fortawesome/free-solid-svg-icons';
+import { useForm } from 'react-hook-form';
+import { joiResolver } from '@hookform/resolvers/joi';
+import Joi from 'joi';
+
+const schema = Joi.object({
+  email: Joi.string()
+    .email({ tlds: { allow: false } })
+    .required()
+    .label('Email'),
+  password: Joi.string().required().required(),
+});
 
 const LoginModal = ({ open, onClose, goToSignUp }) => {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm({ resolver: joiResolver(schema) });
   return (
     <div
       className={`fixed w-full h-[calc(100%-1rem)] max-h-full insert-0 flex justify-center items-center ${
@@ -26,29 +42,50 @@ const LoginModal = ({ open, onClose, goToSignUp }) => {
             <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">
               Sign in to our Platfom
             </h3>
-            <form className="space-y-6" action="#">
+            <form
+              className="space-y-6"
+              onSubmit={handleSubmit((data) => console.log(data))}
+            >
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Your email
                 </label>
+
                 <input
+                  {...register('email')}
                   type="email"
                   name="email"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  className={`${
+                    errors.email ? 'border-red-600 dark:border-red-300' : 'border-gray-300 dark:border-gray-500'
+                  } bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:placeholder-gray-400 dark:text-white`}
                   placeholder="name@company.com"
                 />
+                <p
+                  className='mb1fixed mt-1 text-sm text-red-500 dark:text-red-300'
+                >
+                  {errors.email && errors.email.message}
+                </p>
               </div>
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
                   Your password
                 </label>
                 <input
+                  {...register('password')}
                   type="password"
                   name="password"
                   placeholder="••••••••"
-                  className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white"
+                  className={`${
+                    errors.password ? 'border-red-600 dark:border-red-300' : 'border-gray-300 dark:border-gray-500'
+                  } bg-gray-50 border text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:placeholder-gray-400 dark:text-white`}
                 />
+                <p
+                  className='mb-1fixed mt-1 text-sm text-red-500 dark:text-red-300'
+                >
+                  {errors.password && errors.password.message}
+                </p>
               </div>
+              
               <button type="submit" className="btn-primary w-full py-3">
                 Login to your account
               </button>
