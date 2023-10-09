ab7496db8d5bcfa20c6b37c9fe46c9a99d373b22
Sabbir-Rahman <sabbirrahman42@iut-dhaka.edu>
2023-10-09 10:42:53 +0600

    read more working with seperate page

--- src/Sections/PostDetailsSection.jsx
+++ src/Sections/PostDetailsSection.jsx
@@ -1,8 +1,38 @@
+import { useState, useEffect } from 'react';
 import { useParams } from 'react-router-dom';
+import { getSingleBlogById } from '../services/blogs';
 
 const PostDetailsSection = () => {
   const { blogId } = useParams();
-  return <div>PostDetailsSection, postId : {blogId}</div>;
+  const [blog, setBlog] = useState({});
+
+  useEffect(() => {
+    async function getBlog() {
+      const response = await getSingleBlogById(blogId);
+      if(response.status == 'SUCCESS'){
+        setBlog(response.blog)
+      }else {
+        setBlog({})
+      }
+      
+    }
+    getBlog();
+  }, []);
+
+  return (
+    <>
+      <div className="w-full flex justify-center mt-10">
+        <div className="p-10 w-full flex flex-col jus rounded-lg shadow md:flex-row sm:max-w-xl md:max-w-3xl lg:max-w-4xl xl:max-w-5xl hover:bg-gray-100 dark:border-gray-700 dark:bg-gray-300 dark:bg-opacity-25 dark:hover:bg-gray-700">
+          <div className="text-white mr-auto">
+            <div className="text-white text-2xl">{blog.title}</div>
+            <div className="text-white font-medium">{blog.authorName} | {blog.authorEmail}</div>
+            <div className="text-white italic">{blog.time}</div>
+            <p className='mt-6 text-white font-light'>{blog.body}</p>
+          </div>
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default PostDetailsSection;
--- src/services/blogs.js
+++ src/services/blogs.js
@@ -1,26 +1,50 @@
-import Axios from "./Api/axios";
+import Axios from './Api/axios';
 
 const getBlogs = async () => {
   try {
     const response = await Axios.get('/api/v1/blogs');
-    return { status: 'SUCCESS',blogs: response.data.data}
+    return { status: 'SUCCESS', blogs: response.data.data };
   } catch (err) {
-    return { status: 'ERR', blogs: [], message: err.response.data.message, developerMessage: err.response.data.developerMessage}
+    return {
+      status: 'ERR',
+      blogs: [],
+      message: err.response.data.message,
+      developerMessage: err.response.data.developerMessage,
+    };
+  }
+};
+
+const getSingleBlogById = async (blogId) => {
+  try {
+    const response = await Axios.get(`/api/v1/blogs/${blogId}`);
+    
+    return { status: 'SUCCESS', blog: response.data.data };
+  } catch (err) {
+    return {
+      status: 'ERR',
+      blog: [],
+      message: err.response.data.message,
+      developerMessage: err.response.data.developerMessage,
+    };
   }
 };
 
 const createBlog = async (inputData, accesstoken) => {
-    try {
-      const config = {
-        headers: {
-            Authorization: `Bearer ${accesstoken}`,
-        },
-      }
-      const response = await Axios.post('/api/v1/blogs', inputData, config);
-      return { status: 'SUCCESS',message: response.data.message}
-    } catch (err) {
-      return { status: 'ERROR', message: err.response.data.message, developerMessage: err.response.data.developerMessage}
-    }
-  };
+  try {
+    const config = {
+      headers: {
+        Authorization: `Bearer ${accesstoken}`,
+      },
+    };
+    const response = await Axios.post('/api/v1/blogs', inputData, config);
+    return { status: 'SUCCESS', message: response.data.message };
+  } catch (err) {
+    return {
+      status: 'ERROR',
+      message: err.response.data.message,
+      developerMessage: err.response.data.developerMessage,
+    };
+  }
+};
 
-export { getBlogs, createBlog };
+export { getBlogs, createBlog, getSingleBlogById };
